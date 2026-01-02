57198035d2fd52b5ecbaee3aea16a61b224c3c38
Ismi Nur Fadilah <isminurfadilah9@gmail.com>
2026-01-02 17:51:32 +0700

    Update Shopping Cart

--- praktikum/week7-koleksi-keranjang/src/main/java/com/upb/agripos/ShoppingCart.java
+++ praktikum/week7-koleksi-keranjang/src/main/java/com/upb/agripos/ShoppingCart.java
@@ -3,53 +3,24 @@ package main.java.com.upb.agripos;
 import java.util.ArrayList;
 
 public class ShoppingCart {
-    private ArrayList<Product> products;
+    private final ArrayList<Product> items = new ArrayList<>();
 
-    public ShoppingCart() {
-        products = new ArrayList<>();
-    }
+    public void addProduct(Product p) { items.add(p); }
+    public void removeProduct(Product p) { items.remove(p); }
 
-    // Method addProduct menerima SATU parameter: Product
-    public void addProduct(Product product) {
-        products.add(product);
-        System.out.println("Produk ditambahkan: " + product.toString());
-    }
-
-    public void removeProduct(Product product) {
-        if (products.remove(product)) {
-            System.out.println("Produk dihapus: " + product.toString());
-        } else {
-            System.out.println("Produk tidak ditemukan dalam keranjang");
+    public double getTotal() {
+        double sum = 0;
+        for (Product p : items) {
+            sum += p.getPrice();
         }
+        return sum;
     }
 
-    // Method printCart TANPA parameter
     public void printCart() {
-        System.out.println("\n========== ISI KERANJANG ==========");
-        if (products.isEmpty()) {
-            System.out.println("Keranjang kosong");
-        } else {
-            int total = 0;
-            for (int i = 0; i < products.size(); i++) {
-                Product p = products.get(i);
-                System.out.println((i + 1) + ". " + p.toString());
-                total += p.getPrice();
-            }
-            System.out.println("===================================");
-            System.out.println("Total: Rp " + total);
+        System.out.println("Isi Keranjang:");
+        for (Product p : items) {
+            System.out.println("- " + p.getCode() + " " + p.getName() + " = " + p.getPrice());
         }
-        System.out.println("===================================\n");
-    }
-
-    public int getTotalPrice() {
-        int total = 0;
-        for (Product p : products) {
-            total += p.getPrice();
-        }
-        return total;
-    }
-
-    public int getProductCount() {
-        return products.size();
+        System.out.println("Total: " + getTotal());
     }
 }
