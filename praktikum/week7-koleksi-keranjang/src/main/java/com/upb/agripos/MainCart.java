938a8d343331630fbb53444971cbf6fb4849bdc9
Ismi Nur Fadilah <isminurfadilah9@gmail.com>
2026-01-02 17:51:49 +0700

    Update Main Cart

--- praktikum/week7-koleksi-keranjang/src/main/java/com/upb/agripos/MainCart.java
+++ praktikum/week7-koleksi-keranjang/src/main/java/com/upb/agripos/MainCart.java
@@ -1,23 +1,21 @@
 package main.java.com.upb.agripos;
 
-import main.java.com.upb.InvalidQuantityException;
-
 public class MainCart {
-    public static void main(String[] args) throws InvalidQuantityException, InvalidPriceException {
-        System.out.println("Hello, I am Ismi Nur Fadilah-240202868 (Week7)\n");
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Ismi Nur Fadilah-240202868 (Week7)");
 
         Product p1 = new Product("P01", "Beras", 50000, 0);
         Product p2 = new Product("P02", "Pupuk", 30000, 0);
         Product p3 = new Product("P03", "Furadan", 35000, 0);
 
         ShoppingCart cart = new ShoppingCart();
-        cart.addProduct(p1, 0);
-        cart.addProduct(p2, 0);
-        cart.addProduct(p3, 0);
+        cart.addProduct(p1);
+        cart.addProduct(p2);
+        cart.addProduct(p3);
         cart.printCart();
 
-        System.out.println("Setelah menghapus " + p1.getCode() + " " + p1.getName() + " dari keranjang:");
-        
+        System.out.println("\nSetelah menghapus " + p1.getCode() + " " + p1.getName() + " dari keranjang:");
+        cart.removeProduct(p1);
         cart.printCart();
     }
 }
